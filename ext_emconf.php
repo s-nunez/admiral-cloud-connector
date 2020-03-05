6b6220d90bc2602a965086f88b6b4a0ddceac671
Santiago Nunez Negrillo <s.nuneznegrillo@gmail.com>
2020-03-05 12:04:44 +0100

    [TASK] Add first version of files composer.json and ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+    'title' => 'Admiral cloud connector',
+    'description' => 'Admiral cloud connector',
+    'category' => 'plugin',
+    'author' => 'CPS-IT GmbH',
+    'author_email' => '',
+    'author_company' => 'CPS-IT GmbH',
+    'state' => 'stable',
+    'uploadfolder' => '1',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' =>
+        array (
+            'depends' =>
+                array (
+                    'typo3' => '9.5.0-9.5.99',
+                ),
+            'conflicts' =>
+                array (
+                ),
+            'suggests' =>
+                array (
+                ),
+        ),
+);
