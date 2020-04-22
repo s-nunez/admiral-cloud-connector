4b87e7254ee5bde74ba7d1e68f556105d0f6cd53
Steffen Mevius <s.mevius@familie-redlich.de>
2020-04-22 10:14:59 +0200

    [TASK] add first_name, last_name, security_group to user profile settings, add postmessage for crop modal

--- ext_tables.php
+++ ext_tables.php
@@ -48,5 +48,24 @@ if (TYPO3_MODE === 'BE') {
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_m1.xlf',
         ]
     );
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['first_name'] = [
+        'label' => 'LLL:EXT:admiral_cloud_connector/Resources/Private/Language/locallang_be.xlf:usersettings.first_name',
+        'type' => 'text',
+        'max' => 80,
+        'table' => 'be_users'
+    ];
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['last_name'] = [
+        'label' => 'LLL:EXT:admiral_cloud_connector/Resources/Private/Language/locallang_be.xlf:usersettings.last_name',
+        'type' => 'text',
+        'max' => 80,
+        'table' => 'be_users'
+    ];
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['security_group'] = [
+        'label' => 'LLL:EXT:admiral_cloud_connector/Resources/Private/Language/locallang_be.xlf:usersettings.security_group',
+        'type' => 'text',
+        'max' => 80,
+        'table' => 'be_users'
+    ];
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToUserSettings('--div--;LLL:EXT:admiral_cloud_connector/Resources/Private/Language/locallang_be.xlf:admiral_cloud_connector_title,first_name,last_name,security_group');
 
 }
