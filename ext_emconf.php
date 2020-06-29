b630b8a399acd93e5a751045ed2d90d03432a8b5
Santiago Nunez Negrillo <s.nuneznegrillo@gmail.com>
2020-06-29 14:04:57 +0200

    [RELEASE] Release of admiral_cloud_connector 1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' =>
         array (
             'depends' =>
