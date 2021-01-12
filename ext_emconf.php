e341a84b3eae688f874cdb7cdc242568fde1a58a
smevius <s.mevius@familie-redlich.de>
2021-01-12 16:28:26 +0100

    [RELEASE] Release of admiral_cloud_connector 1.0.13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.12',
+    'version' => '1.0.13',
     'constraints' =>
         array (
             'depends' =>
