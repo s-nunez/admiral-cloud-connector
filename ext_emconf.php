3b9cc29170ecf5f968bbf8b23c0bb4d4ebd097a9
smevius <s.mevius@familie-redlich.de>
2022-09-27 16:39:42 +0200

    [TASK] rais version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.18',
+    'version' => '1.0.20',
     'constraints' =>
         array (
             'depends' =>
