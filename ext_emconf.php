da33291d61e5c55973e6add39e5b60c49ba804e8
smevius <s.mevius@familie-redlich.de>
2022-09-28 13:53:28 +0200

    [TASK] match version with github

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.19',
+    'version' => '1.0.21',
     'constraints' =>
         array (
             'depends' =>
